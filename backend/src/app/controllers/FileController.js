3f2ed0e3950c2129076eb91d3916ab6dd6f2d2a1
Leonardo Monteiro <leosilvamonteiros@gmail.com>
2020-02-19 19:53:09 -0300

    file upload added

---
+++ backend/src/app/controllers/FileController.js
@@ -0,0 +1,20 @@
+import File from '../models/File';
+
+class FileController {
+  async store(req, res) {
+    const { originalname: name, filename: path } = req.file;
+
+    const { id } = await File.create({
+      name,
+      path,
+    });
+
+    return res.json({
+      id,
+      name,
+      path,
+    });
+  }
+}
+
+export default new FileController();
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import multerConfig from './config/multer';
+
+import FileController from './app/controllers/FileController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.get('/', (req, res) => {
   return res.json({ message: 'CantinaApp Initialized' });
 });
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
