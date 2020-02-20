e9f9b382e740b06996adc56ffc71b48f706f36c1
Leonardo Monteiro <leosilvamonteiros@gmail.com>
2020-02-20 00:37:36 -0300

    users model updates

--- backend/src/app/models/User.js
+++ backend/src/app/models/User.js
@@ -2,7 +2,16 @@ import Sequelize, { Model } from 'sequelize';
 
 class User extends Model {
   static init(sequelize) {
-    super.init({}, { sequelize });
+    super.init(
+      {
+        id: Sequelize.INTEGER,
+        email: Sequelize.STRING,
+        password_hash: Sequelize.STRING,
+        balance: Sequelize.FLOAT,
+        operator: Sequelize.BOOLEAN,
+      },
+      { sequelize }
+    );
     return this;
   }
 }
