c22f65a359b7fec9a764d6274780cab153a31290
Leonardo Monteiro <leosilvamonteiros@gmail.com>
2020-02-22 02:04:51 -0300

    cashier controller added

---
+++ backend/src/app/controllers/CashierController.js
@@ -0,0 +1,10 @@
+// import Cashier from '../models/Cashier';
+
+class CashierController {
+  async store(req, res) {
+    // const cashier = await Cashier.create(req.body);
+    return res.json();
+  }
+}
+
+export default new CashierController();
