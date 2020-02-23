a113b4f342a187de3c30d5b9734c4e82cc4b91b5
Leonardo Monteiro <leosilvamonteiros@gmail.com>
2020-02-23 14:56:27 -0300

    operator/user controller updates

--- backend/src/app/controllers/OperatorController.js
+++ backend/src/app/controllers/OperatorController.js
@@ -1,16 +1,28 @@
 import jwt from 'jsonwebtoken';
 
 import User from '../models/User';
+import Core from '../models/Core';
 
 import authConfig from '../../config/auth';
 
 class OperatorController {
   async index(req, res) {
-    return res.json();
+    const operators = await User.findAll({
+      where: { operator: true },
+      attributes: ['id', 'name', 'email', 'balance'],
+      include: [
+        {
+          model: Core,
+          as: 'core',
+          attributes: ['id', 'name'],
+        },
+      ],
+    });
+    return res.json(operators);
   }
 
   async store(req, res) {
-    const { email } = req.body;
+    const { email, core_id } = req.body;
 
     const userExists = await User.findOne({
       where: { email },
@@ -19,6 +31,15 @@ class OperatorController {
     if (userExists) {
       return res.status(400).json({ error: 'User already exists' });
     }
+
+    const coreExists = await Core.findOne({
+      where: { id: core_id },
+    });
+
+    if (!coreExists) {
+      return res.status(401).json({ error: 'Core does not exists' });
+    }
+
     req.body.operator = true;
 
     const { id, name, balance, operator } = await User.create(req.body);
--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -19,7 +19,7 @@ class UserController {
   }
 
   async store(req, res) {
-    const { email } = req.body;
+    const { email, core_id } = req.body;
 
     const userExists = await User.findOne({
       where: { email },
@@ -27,6 +27,14 @@ class UserController {
 
     if (userExists) {
       return res.status(400).json({ error: 'User already exists' });
+    }
+
+    const coreExists = await Core.findOne({
+      where: { id: core_id },
+    });
+
+    if (!coreExists) {
+      return res.status(401).json({ error: 'Core does not exists' });
     }
 
     const { id, name, balance, operator } = await User.create(req.body);
