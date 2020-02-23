ee07c6f4e6c3903d43a1071cdfcfd67e203b045a
Leonardo Monteiro <leosilvamonteiros@gmail.com>
2020-02-23 17:28:03 -0300

    mail sending module added

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '8919689b88c41a',
+    pass: '958a7d7fbb1c96',
+  },
+  default: {
+    from: 'Equipe Cantina App <noreply@cantinapp.com>',
+  },
+};
